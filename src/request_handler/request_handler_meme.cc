0d65f372886aa8e6938cefe40063f80b6a94c8db
Devel User <kennethle95@g.ucla.edu>
2023-06-10 03:47:39 +0000

    Tried to initalize the json object as an empty list but it had no push_back functionality so it crashed again
    Change-Id: I4272624f0d639c77fede23f623515697baa29091

--- src/request_handler/request_handler_meme.cc
+++ src/request_handler/request_handler_meme.cc
@@ -373,7 +373,7 @@ void Request_Handler_Meme::handle_get(std::string meme_path, reply *http_reply){
     write_base_http(http_reply);
 
     meme_data data;
-    auto response_data = json::object();
+    json response_data;
     std::string json_contents;    
     
     std::string uri = meme_path;
@@ -461,7 +461,7 @@ void Request_Handler_Meme::handle_post(std::string meme_path, std::string ip_add
     write_base_http(http_reply);
 
     std::string uri = meme_path;
-    auto json_contents = json::object();
+    json json_contents;
     
     size_t last_slash_pos = uri.find_last_of('/');
 
